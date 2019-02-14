fc4983696c62f32362e4e72b0182b0957ada13d5
GermanRodrickson <germanrodrickson@gmail.com>
2019-02-14 22:20:11 +0100

    first commit

---
+++ vscode.js
@@ -0,0 +1,71 @@
+{
+  "editor.fontFamily": "Operator Mono, Menlo, Monaco, 'Courier New', monospace",
+  "editor.fontSize": 15,
+  "editor.lineHeight": 20,
+  "editor.quickSuggestionsDelay": 500,
+  "editor.letterSpacing": 0.5,
+  "workbench.editor.tabSizing": "shrink",
+  "html.format.enable": true,
+  "html.format.preserveNewLines": true,
+  "files.trimTrailingWhitespace": true,
+  "editor.tabSize": 2,
+  "editor.cursorWidth": 2,
+  // this isn't really underline but we hack it to be a thicker cursor
+  "editor.cursorStyle": "line",
+  "editor.multiCursorModifier": "ctrlCmd",
+  "editor.fontWeight": "400",
+  "editor.cursorBlinking": "blink",
+  "editor.colorDecorators": false,
+  "editor.minimap.enabled": true,
+  //extensions
+  "workbench.activityBar.visible": false,
+  "workbench.sideBar.location": "left",
+  "editor.renderWhitespace": "none",
+  "editor.minimap.showSlider": "always",
+  "workbench.startupEditor": "newUntitledFile",
+  "extensions.ignoreRecommendations": false,
+  "files.exclude": {
+    "**/.git": true,
+    "**/.svn": true,
+    "**/.hg": true,
+    "**/CVS": true,
+    "**/.DS_Store": true,
+    "**/.next": true
+  },
+  "workbench.colorTheme": "Material Theme Darker",
+  "workbench.editor.tabCloseButton": "off",
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact"
+  },
+  // show snippets before any other auto completion
+  "editor.snippetSuggestions": "top",
+  // this lets you quick open multiple items in a row and now have the previous ones closed on you
+  "workbench.editor.enablePreviewFromQuickOpen": false,
+  "window.title": "${dirty} ${activeEditorMedium}${separator}${rootName}",
+  "sublimeTextKeymap.promptV3Features": true,
+  // These are all my auto-save configs
+  "editor.formatOnSave": true,
+  // turn it off for JS
+  // WES TURN THIS BACK ON
+  "[javascript]": {
+    "editor.formatOnSave": true
+  },
+  "eslint.autoFixOnSave": true,
+  "eslint.alwaysShowStatus": true,
+  "editor.autoIndent": true,
+  "window.closeWhenEmpty": true,
+  "editor.detectIndentation": false,
+  "files.insertFinalNewline": true,
+  "editor.showFoldingControls": "always",
+  "editor.find.seedSearchStringFromSelection": true,
+  "editor.matchBrackets": true,
+  "window.openFilesInNewWindow": "off",
+  "workbench.editor.enablePreview": true,
+  "editor.scrollBeyondLastLine": true,
+  "editor.useTabStops": true,
+  "editor.formatOnPaste": true,
+  "window.zoomLevel": 0,
+  "editor.parameterHints.enabled": false,
+  "workbench.iconTheme": "eq-material-theme-icons-light",
+  "colorize.ignore_search_variables_info": true
+}
